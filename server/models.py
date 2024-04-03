a3873dd8b407f867846092cf43211f809bdf40aa
Elise Erickson <elisece717@gmail.com>
2024-04-03 16:49:14 -0400

    creates User model

--- server/models.py
+++ server/models.py
@@ -6,7 +6,21 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception("Cannot access password hash.")
+    
+    @password_hash.setter
+    def password_hash(self, pw):
+        pw_hash = bcrypt.generate_password_hash(pw.encode("utf-8"))
+        self._password_hash = pw_hash.decode("utf-8")
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
